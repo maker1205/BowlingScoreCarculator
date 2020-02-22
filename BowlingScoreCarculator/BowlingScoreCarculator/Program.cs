b8bb20b4d42742ec92fde76489b307bd1dc1304c
maker05 <baejinho05@naver.com>
2020-02-23 00:05:27 +0900

    프로젝트 생성

---
+++ BowlingScoreCarculator/BowlingScoreCarculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BowlingScoreCarculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            Console.WriteLine("10프레임동안 쓰러트린 볼링핀의 개수를 입력해주세요.");
+            string score = Console.ReadLine();
+
+        }
+    }
+}
