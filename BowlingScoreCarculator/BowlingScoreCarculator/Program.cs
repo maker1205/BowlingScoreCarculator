b6db83926a23114171d796b0c1f34f0f9a71a4c4
maker05 <baejinho05@naver.com>
2020-02-23 17:24:43 +0900

    내용 삽입 1차

--- BowlingScoreCarculator/BowlingScoreCarculator/Program.cs
+++ BowlingScoreCarculator/BowlingScoreCarculator/Program.cs
@@ -16,25 +16,43 @@ namespace BowlingScoreCarculator
                 ScoreIn(i);
             }
 
+            Console.WriteLine("계산중입니다......");
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (scores[i] == 10)
+                {
+                    Strike(scores[i + 1], scores[i + 2]);
+                }
+                else
+                {
+
+                }
+            }
         }
 
-        private static int Spare(String score)
+        private static int Spare(int score)
         {
             var sum = 0;
 
             return sum;
         }
 
-        private static int Strike(String score)
+        private static int Strike(int score1,int score2)
         {
-            var sum = 0;
-
-            return sum;
+            var eachFrameSum = 0;
+            var baseScore = 10;
+            eachFrameSum = baseScore + score1 + score2;
+            return eachFrameSum;
         }
 
         private static void ScoreIn(int array)
         {
             scores[array] = Console.Read();
+            if(scores[array] < 0 && scores[array] > 10)
+            {
+                Console.WriteLine("0~10 사이의 점수를 입력해주세요");
+            }
         }
     }
 }
