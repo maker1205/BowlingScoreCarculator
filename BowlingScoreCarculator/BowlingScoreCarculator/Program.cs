a0c1b8fd119f8d28c1ff0e20a0be594783b4e7de
maker05 <baejinho05@naver.com>
2020-02-23 22:15:15 +0900

    완성(임시)

--- BowlingScoreCarculator/BowlingScoreCarculator/Program.cs
+++ BowlingScoreCarculator/BowlingScoreCarculator/Program.cs
@@ -4,11 +4,11 @@ namespace BowlingScoreCarculator
 {
     class Program
     {
-        private static int[] scores;
-
         static void Main(string[] args)
         {
             var sum = 0;
+            int[] scores = new int[12];
+            int cntspr = 0, cntstk = 0;
             
             Console.WriteLine("10프레임동안 쓰러트린 볼링핀의 개수를 하나씩 입력해주세요.");
             for(int i = 0; i < 10; i++)
@@ -16,19 +16,22 @@ namespace BowlingScoreCarculator
                 Console.Write((i + 1) + "프레임");
                 String input = Console.ReadLine();
                 int n = Convert.ToInt32(input);
-                scores[i] = n;
+                
                 if (n < 0 && n > 10)
                 {
                     Console.Write("0에서 10까지의 숫자만 입력 가능합니다.");
                     i = i--;
                 }
+                scores[i] = n;
+                if (n == 10)
+                    cntstk++;
+                else
+                    cntspr++;
             }
 
             Console.WriteLine("나머지 점수는 10점으로 처리됩니다.");
-
-            int v = 10;
-            scores[10] = v;
-            scores[11] = v;
+            scores[10] = 10;
+            scores[11] = 10;
             
             Console.WriteLine("계산중입니다......");
             for (int i = 0; i < 10; i++)
@@ -44,6 +47,8 @@ namespace BowlingScoreCarculator
             }
 
             Console.WriteLine("총 점수의 합계는 " + sum + " 입니다.");
+            Console.WriteLine("스페어 횟수는 " + cntspr + "회 이며");
+            Console.WriteLine("스트라이크 횟수는 " + cntstk + "회 입니다.");
         }
 
         private static int Spare(int score)
